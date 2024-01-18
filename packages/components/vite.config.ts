b3a4e523f1e4cc61a192c720ba8e5d5b4ebc41d1
ohprettyhak <haklee.googl@gmail.com>
2024-01-18 12:40:22 +0900

    feat: tsconfig 옵션 추가 및 의존성 버전 변경

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.ts'),
       name: '@packages/components',
       formats: ['cjs', 'es'],
-      fileName: format => `index.${format}`,
+      fileName: format => `index.${format}.js`,
     },
   },
 });
