f1367c5f1f6c7206921b61d450dadf644961d8be
seongminn <berry9901@gmail.com>
2024-01-29 20:33:59 +0900

    feat: image uploader 구현

--- packages/ui/src/image-uploader/index.tsx
+++ packages/ui/src/image-uploader/index.tsx
@@ -1,3 +1,59 @@
-export default function ImageUploader() {
-  return <div>todo</div>;
+import { ChangeEvent, ReactNode, useRef, useState } from 'react';
+
+interface UploadProps {
+  id?: string;
+  name?: string;
+  onChange?: (file?: File) => void;
+  children?: ((src: string, file?: File) => ReactNode) | ReactNode;
 }
+
+const Upload = ({ id, name, onChange, children }: UploadProps) => {
+  const [file, setFile] = useState<File>();
+  const [src, setSrc] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const encodeFileToBase64 = (file: File) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setSrc(reader.result as string);
+    };
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newFile = e.target.files?.item(0);
+
+    if (!newFile) return;
+
+    setFile(newFile);
+    onChange?.(newFile);
+    encodeFileToBase64(newFile);
+
+    if (!inputRef.current) return;
+
+    inputRef.current.value = '';
+  };
+
+  const handleFileClick = () => {
+    if (!inputRef.current) return;
+
+    inputRef.current.click();
+  };
+
+  return (
+    <div className="inline-block" onClick={handleFileClick} role="presentation">
+      <input
+        id={id}
+        name={name}
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={handleFileChange}
+        hidden
+      />
+      {typeof children === 'function' ? children(src, file) : children}
+    </div>
+  );
+};
+
+export default Upload;
