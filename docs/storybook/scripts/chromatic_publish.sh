5949f53a060cdd1f33f7bee494d54ed7bee055a7
seongminn <berry9901@gmail.com>
2024-01-30 17:56:47 +0900

    fix: chromatic token name 변경

--- docs/storybook/scripts/chromatic_publish.sh
+++ docs/storybook/scripts/chromatic_publish.sh
@@ -1,2 +1,2 @@
-CHROMATIC_TOKEN=$(grep CHROMATIC_TOKEN .env | cut -d "=" -f2)
-pnpm dlx chromatic --project-token=$CHROMATIC_TOKEN --exit-once-uploaded --allow-console-errors --build-script-name=build
+CHROMATIC_PROJECT_TOKEN=$(grep CHROMATIC_PROJECT_TOKEN .env | cut -d "=" -f2)
+pnpm dlx chromatic --project-token=$CHROMATIC_PROJECT_TOKEN --exit-once-uploaded --allow-console-errors --build-script-name=build
