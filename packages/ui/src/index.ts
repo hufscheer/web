27c2fc6aeecd4fdd902777d7f4afd7f3656928f9
seongminn <berry9901@gmail.com>
2024-02-13 01:32:23 +0900

    feat: Icon 컴포넌트 구현

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Uploader } from './image-uploader';
 export { default as Input } from './input';
 export { default as Toast } from './toast';
+export { default as Icon } from './icon';
