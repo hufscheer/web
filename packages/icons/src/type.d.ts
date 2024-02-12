380e3924f503937796a894a60b493d1740471174
seongminn <berry9901@gmail.com>
2024-02-13 01:31:47 +0900

    feat: svg 파일용 workspace 추가 - @hcc/icons

---
+++ packages/icons/src/type.d.ts
@@ -0,0 +1,11 @@
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+
+  const source: ReactComponent;
+
+  export default source;
+}
