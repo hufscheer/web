7dda273c229e7010f2723196e96318656f9c184c
seongminn <berry9901@gmail.com>
2024-02-26 01:43:33 +0900

    chore: 루미큐브 cheer talk 임시 저장

---
+++ apps/spectator/queries/useRummiCheerMutation/query.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { postCheer } from '@/api/game';
+
+export default function useRummiCheerMutation({
+  gameTeamId,
+  gameId,
+}: {
+  gameTeamId: number;
+  gameId: string;
+}) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ['cheer'],
+    mutationFn: (cheerCount: number) =>
+      postCheer({ gameId: gameId, gameTeamId, cheerCount }),
+    onMutate: async () => {
+      const previousCount = queryClient.getQueryData(['game-cheer', gameId]);
+
+      return { previousCount };
+    },
+    retry: false,
+  });
+}
