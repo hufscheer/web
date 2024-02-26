f6ab00f92de86409a3977f7df43c24b88da80b3f
seongminn <berry9901@gmail.com>
2024-02-26 10:47:42 +0900

    style: icon color 속성 지정

--- packages/ui/src/icon/icon.css.ts
+++ packages/ui/src/icon/icon.css.ts
@@ -7,11 +7,26 @@ export const icon = recipe({
   },
   variants: {
     color: {
-      primary: { fill: theme.colors.primary[3] },
-      secondary: { fill: theme.colors.secondary[3] },
-      gray: { fill: theme.colors.gray[3] },
-      error: { fill: theme.colors.indicatorRed[3] },
-      success: { fill: theme.colors.indicatorGreen[3] },
+      primary: {
+        fill: theme.colors.primary[3],
+        color: theme.colors.primary[3],
+      },
+      secondary: {
+        fill: theme.colors.secondary[3],
+        color: theme.colors.primary[3],
+      },
+      gray: {
+        fill: theme.colors.gray[3],
+        color: theme.colors.primary[3],
+      },
+      error: {
+        fill: theme.colors.indicatorRed[3],
+        color: theme.colors.primary[3],
+      },
+      success: {
+        fill: theme.colors.indicatorGreen[3],
+        color: theme.colors.primary[3],
+      },
     },
   },
 });
