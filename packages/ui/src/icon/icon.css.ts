894ba3dbc7805a26b83a0c55ca896d46c461a3c9
seongminn <berry9901@gmail.com>
2024-02-26 10:56:09 +0900

    chore: 필요 없는 스타일 속성 제거

--- packages/ui/src/icon/icon.css.ts
+++ packages/ui/src/icon/icon.css.ts
@@ -2,31 +2,13 @@ import { theme } from '@hcc/styles';
 import { recipe } from '@vanilla-extract/recipes';
 
 export const icon = recipe({
-  base: {
-    fill: 'currentColor',
-  },
   variants: {
     color: {
-      primary: {
-        fill: theme.colors.primary[3],
-        color: theme.colors.primary[3],
-      },
-      secondary: {
-        fill: theme.colors.secondary[3],
-        color: theme.colors.primary[3],
-      },
-      gray: {
-        fill: theme.colors.gray[3],
-        color: theme.colors.primary[3],
-      },
-      error: {
-        fill: theme.colors.indicatorRed[3],
-        color: theme.colors.primary[3],
-      },
-      success: {
-        fill: theme.colors.indicatorGreen[3],
-        color: theme.colors.primary[3],
-      },
+      primary: { color: theme.colors.primary[3] },
+      secondary: { color: theme.colors.primary[3] },
+      gray: { color: theme.colors.primary[3] },
+      error: { color: theme.colors.primary[3] },
+      success: { color: theme.colors.primary[3] },
     },
   },
 });
