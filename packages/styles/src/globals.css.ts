42c4b46e8cad192384a755d81fae860c7ff8877c
Hak Lee <haklee.googl@gmail.com>
2024-03-06 21:45:48 +0900

    [FIX] 폰트 OTS 오류 해결 (#58)

--- packages/styles/src/globals.css.ts
+++ packages/styles/src/globals.css.ts
@@ -1,7 +1,7 @@
 import './layers.css';
 import './reset.css';
 
-import { globalFontFace, globalStyle } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
 
 import { theme } from './theme.css';
 
@@ -11,10 +11,6 @@ globalStyle('body', {
   paddingBottom: 'env(safe-area-inset-bottom)',
 });
 
-globalFontFace('Pretendard', {
-  src: 'url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css")',
-});
-
-globalFontFace('Pretendard Variable', {
-  src: 'url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css")',
+globalStyle('div, span, p, a, button, ul, li', {
+  fontFamily: theme.fonts.body,
 });
