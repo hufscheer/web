343c3c50b8333223513c4b96c107ee05ffa84125
seongminn <berry9901@gmail.com>
2024-03-08 01:49:58 +0900

    feat: card 컴포넌트 추가

---
+++ apps/manager/components/Card/Card.css.ts
@@ -0,0 +1,71 @@
+import { rem, theme } from '@hcc/styles';
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+export const root = recipe({
+  base: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: theme.spaces.xs,
+
+    paddingInline: theme.spaces.default,
+    borderRadius: rem(12),
+
+    backgroundColor: theme.colors.background.normal,
+
+    boxShadow: theme.shadows.base,
+  },
+  variants: {
+    paddingVertical: {
+      sm: {
+        paddingBlock: theme.spaces.sm,
+      },
+      md: {
+        paddingBlock: theme.spaces.default,
+      },
+    },
+  },
+});
+
+export const content = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.spaces.default,
+
+  width: '100%',
+});
+
+export const inner = style({
+  display: 'flex',
+  alignItems: 'center',
+  flex: 1,
+});
+
+export const title = recipe({
+  base: { ...theme.textVariants.default },
+  variants: {
+    text: {
+      bold: {
+        fontWeight: theme.textVariants.lg.fontWeight,
+      },
+      normal: {
+        fontWeight: theme.textVariants.sm.fontWeight,
+      },
+    },
+  },
+});
+
+export const subContent = style({
+  color: theme.colors.gray[4],
+  ...theme.textVariants.sm,
+});
+
+export const footer = style({
+  display: 'grid',
+  gridAutoFlow: 'column',
+  gap: theme.spaces.xs,
+
+  width: '100%',
+  paddingTop: theme.spaces.xs,
+});
---
+++ apps/manager/components/Card/index.tsx
@@ -0,0 +1,98 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
+
+import * as styles from './Card.css';
+
+interface CardProps extends ComponentPropsWithoutRef<'div'> {
+  paddingVertical?: 'sm' | 'md';
+  children: React.ReactNode;
+}
+
+const Card = forwardRef<HTMLDivElement, CardProps>(function Card(
+  { paddingVertical = 'md', children, ...props },
+  ref,
+) {
+  return (
+    <div ref={ref} className={styles.root({ paddingVertical })} {...props}>
+      {children}
+    </div>
+  );
+});
+
+interface CardContentProps extends ComponentPropsWithoutRef<'div'> {
+  left?: React.ReactNode;
+  right?: React.ReactNode;
+}
+
+const CardContent = forwardRef<HTMLDivElement, CardContentProps>(
+  function CardContent({ left, right, children, ...props }, ref) {
+    return (
+      <div ref={ref} className={styles.content} {...props}>
+        {left}
+        <div className={styles.inner}>{children}</div>
+        {right}
+      </div>
+    );
+  },
+);
+
+interface CardActionProps extends ComponentPropsWithoutRef<'button'> {}
+
+const CardAction = forwardRef<HTMLButtonElement, CardActionProps>(
+  function CardAction({ children, ...props }, ref) {
+    return (
+      <button ref={ref} {...props}>
+        {children}
+      </button>
+    );
+  },
+);
+
+interface CardTitleProps extends ComponentPropsWithoutRef<'h3'> {
+  text?: 'bold' | 'normal';
+}
+
+const CardTitle = forwardRef<HTMLHeadingElement, CardTitleProps>(
+  function CardTitle({ children, text = 'normal', ...props }, ref) {
+    return (
+      <h3 ref={ref} {...props} className={styles.title({ text })}>
+        {children}
+      </h3>
+    );
+  },
+);
+
+interface CardSubContentProps extends ComponentPropsWithoutRef<'span'> {}
+
+const CardSubContent = forwardRef<HTMLSpanElement, CardSubContentProps>(
+  function CardSubContent({ children, ...props }, ref) {
+    return (
+      <span ref={ref} {...props} className={styles.subContent}>
+        {children}
+      </span>
+    );
+  },
+);
+
+interface CardFooterProps extends ComponentPropsWithoutRef<'div'> {}
+
+const CardFooter = forwardRef<HTMLDivElement, CardFooterProps>(
+  function CardFooter({ children, ...props }, ref) {
+    return (
+      <div ref={ref} {...props} className={styles.footer}>
+        {children}
+      </div>
+    );
+  },
+);
+
+export default Object.assign(
+  {},
+  {
+    Root: Card,
+    Content: CardContent,
+    Action: CardAction,
+    Title: CardTitle,
+    SubContent: CardSubContent,
+    Footer: CardFooter,
+  },
+);
