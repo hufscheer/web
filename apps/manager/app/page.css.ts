340e2e68aaae94491f8500fdbe72eb02f2ec701c
seongminn <berry9901@gmail.com>
2024-03-11 03:01:15 +0900

    chore: 미리보기 UI 추가

---
+++ apps/manager/app/page.css.ts
@@ -0,0 +1,7 @@
+import { theme } from '@hcc/styles';
+import { style } from '@vanilla-extract/css';
+
+export const customButtonStyle = style({
+  backgroundColor: theme.colors.gray[2],
+  color: theme.colors.gray[6],
+});
--- apps/manager/app/page.tsx
+++ apps/manager/app/page.tsx
@@ -1,11 +1,24 @@
-import Link from 'next/link';
+import { Button, Select } from '@mantine/core';
+
+import * as styles from './page.css';
 
 export default function Page(): JSX.Element {
   return (
     <main style={{ display: 'flex', flexDirection: 'column' }}>
       훕치치 매니저
-      <Link href="/league">대회 관리</Link>
-      <Link href="/game">게임 관리</Link>
+      <Select
+        label="라운드"
+        placeholder="골라봐"
+        data={['16강', '8강', '4강', '결승']}
+      />
+      <Button>버튼</Button>
+      <Button variant="light">버튼</Button>
+      <Button variant="subtle">버튼</Button>
+      <Button variant="gradient">버튼</Button>
+      <Button variant="outline">버튼</Button>
+      <Button variant="transparent">버튼</Button>
+      <Button variant="white">버튼</Button>
+      <Button className={styles.customButtonStyle}>버튼</Button>
     </main>
   );
 }
