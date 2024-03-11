6b31efdca9d639f7a5da2723f8b60aa4904617ad
Seongmin Choi <88662637+seongminn@users.noreply.github.com>
2024-03-12 01:07:18 +0900

    [FEAT] manager login 기능 구현 (#73)
    * build: @mantine/form 설치
    
    * feat: login mutation 생성
    
    * feat: login page 생성
    
    * style: page layout 설정

--- apps/manager/app/login/page.tsx
+++ apps/manager/app/login/page.tsx
@@ -1,3 +1,63 @@
+'use client';
+
+import { Box, Button, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import useLoginMutation from '@/hooks/mutations/useLoginMutation';
+
+import * as styles from './page.css';
+
 export default function Login() {
-  return <>로그인</>;
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate: {
+      email: value => (value.includes('@') ? null : 'Invalid email'),
+      password: value => (value.length >= 4 ? null : 'Password is too short'),
+    },
+    validateInputOnChange: true,
+  });
+
+  const { mutate: mutateLogin } = useLoginMutation();
+  const handleSubmit = (values: typeof form.values) => {
+    mutateLogin(values, {
+      onSuccess: () => {
+        const redirect = params.get('redirect');
+
+        if (redirect) router.replace(redirect);
+        else router.replace('/');
+      },
+    });
+  };
+
+  return (
+    <Box>
+      <form onSubmit={form.onSubmit(handleSubmit)} className={styles.page}>
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="이메일을 입력해주세요."
+          {...form.getInputProps('email')}
+        />
+
+        <TextInput
+          type="password"
+          withAsterisk
+          label="Password"
+          placeholder="비밀번호을 입력해주세요."
+          {...form.getInputProps('password')}
+        />
+
+        <Button mt="md" fullWidth type="submit">
+          로그인
+        </Button>
+      </form>
+    </Box>
+  );
 }
--- apps/manager/app/page.tsx
+++ apps/manager/app/page.tsx
@@ -1,24 +1,13 @@
-import { Button, Select } from '@mantine/core';
-
-import * as styles from './page.css';
+import Link from 'next/link';
 
 export default function Page(): JSX.Element {
   return (
     <main style={{ display: 'flex', flexDirection: 'column' }}>
-      훕치치 매니저
-      <Select
-        label="라운드"
-        placeholder="골라봐"
-        data={['16강', '8강', '4강', '결승']}
-      />
-      <Button>버튼</Button>
-      <Button variant="light">버튼</Button>
-      <Button variant="subtle">버튼</Button>
-      <Button variant="gradient">버튼</Button>
-      <Button variant="outline">버튼</Button>
-      <Button variant="transparent">버튼</Button>
-      <Button variant="white">버튼</Button>
-      <Button className={styles.customButtonStyle}>버튼</Button>
+      <Link
+        href={{ pathname: '/login', query: { redirectPath: 'redirectUrl' } }}
+      >
+        로그인 페이지로
+      </Link>
     </main>
   );
 }
