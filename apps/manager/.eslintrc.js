8eb61bbfaf81b173103361e6f2022a2ff29f0cad
ohprettyhak <haklee.googl@gmail.com>
2024-03-13 03:02:39 +0900

    chore: eslint 파일 롤백

--- apps/manager/.eslintrc.js
+++ apps/manager/.eslintrc.js
@@ -1,9 +1,9 @@
-/** @type {import("eslint").Linter.Config} */
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: ['@hcc/eslint-config/next.js'],
   parser: '@typescript-eslint/parser',
-  ignorePatterns: ['next.config.js', 'postcss.config.js', '_redirects'],
+  ignorePatterns: ['next.config.js', 'postcss.config.js'],
   parserOptions: {
     project: true,
   },
