150acb954d150fb723f066485d049dd81571915f
seongminn <berry9901@gmail.com>
2024-03-12 17:11:07 +0900

    style: margin top 제거

--- apps/manager/components/AddButton/AddButton.css.ts
+++ apps/manager/components/AddButton/AddButton.css.ts
@@ -2,7 +2,6 @@ import { theme } from '@hcc/styles';
 import { style } from '@vanilla-extract/css';
 
 export const addButton = style({
-  marginTop: theme.spaces.sm,
   paddingBlock: theme.spaces.xxs,
   backgroundColor: theme.colors.background.normal,
 
