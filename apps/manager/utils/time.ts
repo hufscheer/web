aca5197078ac75b2343dbed0c853d7f934bbe0b8
seongminn <berry9901@gmail.com>
2024-03-12 17:08:47 +0900

    refactor: formatTime 함수에 locale 매개변수 추가

--- apps/manager/utils/time.ts
+++ apps/manager/utils/time.ts
@@ -3,6 +3,10 @@ import 'dayjs/locale/ko';
 
 locale('ko');
 
-export const formatTime = (time: string | Date, format: string) => {
-  return dayjs(time).format(format);
+export const formatTime = (
+  time: string | Date,
+  format: string,
+  locale = 'ko',
+) => {
+  return dayjs(time).locale(locale).format(format);
 };
