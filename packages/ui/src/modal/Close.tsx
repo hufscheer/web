2240dc5c8b5b85f042c953de6fcd99197a6cfa0e
portlogics-mingyukwak <mg.kwak@portlogics.com>
2024-03-12 22:18:00 +0900

    fix: handleOnClick 정상 적용

--- packages/ui/src/modal/Close.tsx
+++ packages/ui/src/modal/Close.tsx
@@ -1,6 +1,6 @@
 import { CrossIcon } from '@hcc/icons';
 import { clsx } from 'clsx';
-import React, { ElementType, MouseEvent, forwardRef } from 'react';
+import React, { ElementType, forwardRef } from 'react';
 
 import { useModal } from './hooks';
 import * as styles from './styles.css';
@@ -18,12 +18,15 @@ type CloseType = <C extends ElementType = 'button'>(
 
 const ModalClose: CloseType = forwardRef(function ModalClose<
   C extends ElementType = 'button',
->({ as, children, className, ...props }: Props<C>, ref: PolymorphicRef<C>) {
+>(
+  { as, children, className, onClick, ...props }: Props<C>,
+  ref: PolymorphicRef<C>,
+) {
   const Component = as || 'button';
   const { onOpenChange } = useModal();
 
   const handleOnClick = () => {
-    if (props.onClick) props.onClick();
+    if (onClick) onClick();
     onOpenChange(false);
   };
 
@@ -44,7 +47,7 @@ const ModalClose: CloseType = forwardRef(function ModalClose<
     <Component
       ref={ref}
       {...props}
-      onClick={() => onOpenChange(false)}
+      onClick={handleOnClick}
       className={clsx(styles.close, className)}
     >
       <Icon source={CrossIcon} size="xs" />
