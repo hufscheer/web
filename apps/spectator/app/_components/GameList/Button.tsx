f4d24c990965919c6719f2b8d9eb50679493a9a9
portlogics-mingyukwak <mg.kwak@portlogics.com>
2024-03-12 22:34:14 +0900

    style: 예정 경기카드 Link 터치 영역 개선

--- apps/spectator/app/_components/GameList/Button.tsx
+++ apps/spectator/app/_components/GameList/Button.tsx
@@ -11,7 +11,7 @@ type GameButtonProps = {
 
 export default function GameButton({ id, state }: GameButtonProps) {
   if (state === 'scheduled') {
-    return <div className={styles.gameButtonArea.root}></div>;
+    return <></>;
   }
 
   return (
