bd33ee53066848ff360d8b078f745a78ddc85b2a
hiimkwak <mindol0320@naver.com>
2024-03-13 22:32:08 +0900

    chore: ul > li 마크업 구조 정상화

--- apps/spectator/app/_components/GameList/Card.tsx
+++ apps/spectator/app/_components/GameList/Card.tsx
@@ -15,14 +15,14 @@ export default function GameCard({ info, state }: GameCardProps) {
   const { id, gameTeams, gameName, startTime } = info;
 
   return (
-    <div className={styles.cardRoot[state]}>
+    <li className={styles.cardRoot[state]}>
       <GameMetadata state={state} gameName={gameName} startTime={startTime} />
 
       <div className={styles.gameContentArea}>
         <GameInfo gameTeams={gameTeams} gameId={id} state={state} />
         <GameButton id={id} state={state} />
       </div>
-    </div>
+    </li>
   );
 }
 
