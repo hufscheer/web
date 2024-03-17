3fe496b278abff55deed3094088cf4ca38e0bfbe
seongminn <berry9901@gmail.com>
2024-03-17 21:47:12 +0900

    fix: 변경된 api 명세에 따른 수정

--- apps/manager/hooks/queries/useLeagueDetailQuery.ts
+++ apps/manager/hooks/queries/useLeagueDetailQuery.ts
@@ -3,7 +3,9 @@ import useLeagueQuery from './useLeagueQuery';
 export default function useLeagueDetailQuery(leagueId: number) {
   const { data: leagues } = useLeagueQuery();
 
-  const league = leagues?.find(league => league.leagueId === leagueId);
+  const league = Object.values(leagues)
+    .flat()
+    ?.find(league => league.leagueId === leagueId);
 
   return { data: league };
 }
