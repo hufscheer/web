465a4dec46462a9a0593209bb539e5dce76c51e6
seongminn <berry9901@gmail.com>
2024-03-17 20:42:56 +0900

    feat: manager 메인페이지 생성

--- apps/manager/app/page.tsx
+++ apps/manager/app/page.tsx
@@ -1,10 +1,22 @@
+'use client';
+
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 import Layout from '@/components/Layout';
+import useLeagueQuery from '@/hooks/queries/useLeagueQuery';
+
+import PlayingCard from './_components/PlayingCard';
 
 export default function Page() {
+  const { data: leagues } = useLeagueQuery();
+
   return (
     <Layout navigationVisible={false}>
+      <Suspense fallback={<div>로딩</div>}>
+        <PlayingCard leagues={leagues.playing} />
+      </Suspense>
+
       <Link
         href={{ pathname: '/login', query: { redirectPath: 'redirectUrl' } }}
       >
