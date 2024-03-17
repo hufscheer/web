73c9b36b0b589816d6d8a7ab85918395ae1c82ac
seongminn <berry9901@gmail.com>
2024-03-17 20:39:10 +0900

    feat: 진행 경기 목록 컴포넌트 생성

---
+++ apps/manager/app/_components/PlayingCard/index.tsx
@@ -0,0 +1,69 @@
+import { CaretDownIcon } from '@hcc/icons';
+import { Icon } from '@hcc/ui';
+import { Button, Flex, Title } from '@mantine/core';
+import Link from 'next/link';
+import { Fragment } from 'react';
+
+import Card from '@/components/Card';
+import useGamesQuery from '@/hooks/queries/useGamesQuery';
+import { LeagueType } from '@/types/league';
+import { formatTime } from '@/utils/time';
+
+import * as styles from './PlayingCard.css';
+
+type PlayingCardProps = {
+  leagues: LeagueType[];
+};
+
+export default function PlayingCard({ leagues }: PlayingCardProps) {
+  const playingLeagues = useGamesQuery(leagues);
+
+  return (
+    <>
+      {playingLeagues.map(({ data, leagueName }) => (
+        <Fragment key={leagueName}>
+          <Title order={3} mb="xs">
+            {leagueName}
+          </Title>
+          <Flex direction="column" gap="xs" mb="sm">
+            {data.map(game => (
+              <Card.Root key={game.id}>
+                <Card.Content component={Link} href={`game/${game.id}`}>
+                  <div style={{ flex: 1 }}>
+                    <Card.Title text="semibold">{game.gameName}</Card.Title>
+                    <Card.SubContent>
+                      {formatTime(game.startTime, 'YYYY.MM.DD')}
+                    </Card.SubContent>
+                  </div>
+                  <Card.Action>
+                    <Icon source={CaretDownIcon} className={styles.caret} />
+                  </Card.Action>
+                </Card.Content>
+                <Card.Footer>
+                  <Flex gap="xs">
+                    <Button
+                      fullWidth
+                      component={Link}
+                      variant="light"
+                      href={`/game/${game.id}/timeline`}
+                    >
+                      타임 라인
+                    </Button>
+                    <Button
+                      fullWidth
+                      component={Link}
+                      variant="light"
+                      href={`/game/${game.id}/cheer-talk`}
+                    >
+                      응원톡
+                    </Button>
+                  </Flex>
+                </Card.Footer>
+              </Card.Root>
+            ))}
+          </Flex>
+        </Fragment>
+      ))}
+    </>
+  );
+}
