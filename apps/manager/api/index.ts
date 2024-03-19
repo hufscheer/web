9aa096dc1595002f5fc6f75e1991670acbe11f2f
Seongmin Choi <88662637+seongminn@users.noreply.github.com>
2024-03-19 18:26:46 +0900

    [FEAT] 유저 인증 정보에 따른 리다이렉션 (#104)
    * feat: cookie 정보에 따른 redirection
    
    * chore: 쿠키 네임 변경

--- apps/manager/api/index.ts
+++ apps/manager/api/index.ts
@@ -1,28 +1,11 @@
-import axios, { InternalAxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
 const instance = axios.create({
   baseURL: '/api',
   headers: {
-    Authorization: `Bearer `,
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
-
-const getAccessToken = () => {
-  if (typeof window !== 'undefined') {
-    const tokenInLocalStorage = localStorage.getItem('accessToken');
-
-    if (tokenInLocalStorage) return tokenInLocalStorage;
-  } else return null;
-};
-
-const onRequest = (config: InternalAxiosRequestConfig) => {
-  const existedToken = getAccessToken();
-  config.headers.Authorization = `Bearer ${existedToken}`;
-
-  return config;
-};
-
-instance.interceptors.request.use(onRequest);
 
 export default instance;
