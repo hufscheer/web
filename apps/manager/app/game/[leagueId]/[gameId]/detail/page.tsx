53effff449c3b0a27517952b1a2b38de4d225fef
ohprettyhak <haklee.googl@gmail.com>
2024-03-22 02:13:21 +0900

    feat: form 태그 추가

--- apps/manager/app/game/[leagueId]/[gameId]/detail/page.tsx
+++ apps/manager/app/game/[leagueId]/[gameId]/detail/page.tsx
@@ -3,7 +3,7 @@ import { Button, Flex, Select, Text, TextInput } from '@mantine/core';
 import { DateTimePicker } from '@mantine/dates';
 import { useForm } from '@mantine/form';
 import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 
 import Layout from '@/components/Layout';
 import { GAMES } from '@/constants/games';
@@ -58,91 +58,94 @@ export default function GameDetail() {
 
   const { mutate: updateGameMutation } = useUpdateGameMutation();
 
-  const handleEdit = () => {
-    if (!edit) {
-      setEdit(!edit);
-      return;
-    }
-    const payload: GameUpdatePayload = {
-      sportsId: Number(form.values.sportsId),
-      gameName: form.values.gameName,
-      videoId: form.values.videoId || null,
-      round: Number(form.values.round),
-      startTime: form.values.startTime.toISOString(),
-      gameQuarter: form.values.gameQuarter,
-      state: form.values.state as 'playing' | 'scheduled' | 'finished',
-    };
+  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
 
-    updateGameMutation({ gameId, payload });
+    if (edit) {
+      const values = form.values;
+      const payload: GameUpdatePayload = {
+        sportsId: Number(values.sportsId),
+        gameName: values.gameName,
+        videoId: values.videoId || null,
+        round: Number(values.round),
+        startTime: values.startTime.toISOString(),
+        gameQuarter: values.gameQuarter,
+        state: values.state as 'playing' | 'scheduled' | 'finished',
+      };
+
+      updateGameMutation({ gameId, payload });
+    }
     setEdit(!edit);
   };
 
   return (
-    <Layout
-      navigationTitle="기본 정보"
-      navigationMenu={
-        <Button variant="subtle" onClick={handleEdit}>
-          {edit ? '완료' : '편집'}
-        </Button>
-      }
-    >
-      <Flex direction="column" gap="sm">
-        <Text>경기 정보</Text>
-        <TextInput
-          label="명칭"
-          disabled={!edit}
-          {...form.getInputProps('gameName')}
-        />
-        <Select
-          placeholder="라운드"
-          data={GAMES.ROUND}
-          checkIconPosition="right"
-          disabled={!edit}
-          {...form.getInputProps('round')}
-        />
-        <Select
-          placeholder="종목"
-          data={GAMES.SPORTS}
-          checkIconPosition="right"
-          disabled={!edit}
-          {...form.getInputProps('sportsId')}
-        />
+    <form onSubmit={handleSubmit}>
+      <Layout
+        navigationTitle="기본 정보"
+        navigationMenu={
+          <Button variant="subtle" type="submit">
+            {edit ? '완료' : '편집'}
+          </Button>
+        }
+      >
+        <Flex direction="column" gap="sm">
+          <Text>경기 정보</Text>
+          <TextInput
+            label="명칭"
+            disabled={!edit}
+            {...form.getInputProps('gameName')}
+          />
+          <Select
+            placeholder="라운드"
+            data={GAMES.ROUND}
+            checkIconPosition="right"
+            disabled={!edit}
+            {...form.getInputProps('round')}
+          />
+          <Select
+            placeholder="종목"
+            data={GAMES.SPORTS}
+            checkIconPosition="right"
+            disabled={!edit}
+            {...form.getInputProps('sportsId')}
+          />
 
-        <Text>참가 팀</Text>
-        {teams &&
-          teams.map((team, index) => (
-            <Text key={team.gameTeamId}>
-              {index + 1}팀 - {team.gameTeamName}
-            </Text>
-          ))}
+          <Text>참가 팀</Text>
+          {teams &&
+            teams.map((team, index) => (
+              <Text key={team.gameTeamId}>
+                {index + 1}팀 - {team.gameTeamName}
+              </Text>
+            ))}
 
-        <Text>경기 정보</Text>
-        <Select
-          placeholder="상황"
-          data={GAMES.STATE}
-          checkIconPosition="right"
-          disabled={!edit}
-          {...form.getInputProps('state')}
-        />
-        <TextInput
-          label="쿼터"
-          disabled={!edit}
-          {...form.getInputProps('gameQuarter')}
-        />
-        <DateTimePicker
-          label="시작"
-          valueFormat="YYYY.MM.DD HH:mm"
-          placeholder="2000.00.00 00:00"
-          disabled={!edit}
-          {...form.getInputProps('startTime')}
-        />
-        <Text>영상</Text>
-        <TextInput
-          label="URL"
-          disabled={!edit}
-          {...form.getInputProps('videoId')}
-        />
-      </Flex>
-    </Layout>
+          <Text>경기 정보</Text>
+          <Select
+            placeholder="상황"
+            data={GAMES.STATE}
+            checkIconPosition="right"
+            disabled={!edit}
+            {...form.getInputProps('state')}
+          />
+          <TextInput
+            label="쿼터"
+            disabled={!edit}
+            {...form.getInputProps('gameQuarter')}
+          />
+          <DateTimePicker
+            label="시작"
+            valueFormat="YYYY.MM.DD HH:mm"
+            placeholder="2000.00.00 00:00"
+            disabled={!edit}
+            {...form.getInputProps('startTime')}
+          />
+          <Text>영상</Text>
+          <TextInput
+            label="URL"
+            disabled={!edit}
+            {...form.getInputProps('videoId')}
+          />
+        </Flex>
+      </Layout>
+    </form>
   );
 }
