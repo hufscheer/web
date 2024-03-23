51d6848135b0f86fee0e2f961d28edb8e9e136f0
seongminn <berry9901@gmail.com>
2024-03-23 22:34:25 +0900

    feat: 종료된 경기의 영상 버튼 추가

--- apps/spectator/app/_components/GameList/Button.tsx
+++ apps/spectator/app/_components/GameList/Button.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+import { GAME_STATE, TABS_CONFIG } from '@/constants/configs';
 import { GameState } from '@/types/game';
 
 import * as styles from './GameList.css';
@@ -10,13 +11,13 @@ type GameButtonProps = {
 };
 
 export default function GameButton({ id, state }: GameButtonProps) {
-  if (state === 'scheduled') {
+  if (state === GAME_STATE.SCHEDULED) {
     return null;
   }
 
   return (
     <div className={styles.gameButtonArea.root}>
-      {state === 'playing' && (
+      {state === GAME_STATE.PLAYING && (
         <Link
           href={{ pathname: `/game/${id}`, query: { cheer: 'open' } }}
           className={styles.gameButtonArea.cheer}
@@ -24,8 +25,23 @@ export default function GameButton({ id, state }: GameButtonProps) {
           응원
         </Link>
       )}
+      {state === GAME_STATE.FINISHED && (
+        <Link
+          href={{
+            pathname: `/game/${id}`,
+            query: { tab: TABS_CONFIG.HIGHLIGHT },
+          }}
+          className={styles.gameButtonArea.record}
+        >
+          영상
+        </Link>
+      )}
+
       <Link
-        href={{ pathname: `/game/${id}`, query: { tab: 'timeline' } }}
+        href={{
+          pathname: `/game/${id}`,
+          query: { tab: TABS_CONFIG.TIMELINE },
+        }}
         className={styles.gameButtonArea.record}
       >
         기록
