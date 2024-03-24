1b1bd2419a6212734f909107bda05f04d0404cb4
HiImConan <mindol0320@naver.com>
2024-03-24 16:31:33 +0900

    style: 관객앱 응원톡 타임라인 UI

--- apps/spectator/app/game/[id]/_components/CheerTalk/Timeline/Timeline.css.ts
+++ apps/spectator/app/game/[id]/_components/CheerTalk/Timeline/Timeline.css.ts
@@ -1,11 +1,71 @@
 import { rem, theme } from '@hcc/styles';
-import { style } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
 
 export const wrapper = style({
   display: 'flex',
-  flexShrink: 0,
-  height: rem(42),
   justifyContent: 'center',
   alignItems: 'center',
   borderBottom: `${rem(1)} solid ${theme.colors.gray[2]}`,
+});
+
+export const timeline = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: theme.spaces.xs,
+  gap: theme.spaces.lg,
+  width: '100%',
+});
+
+export const timestamp = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  borderRadius: rem(4),
+  padding: `${rem(2)} ${rem(4)}`,
+  ...theme.textVariants.xs,
+  fontWeight: 600,
+  color: theme.colors.gray[6],
+  backgroundColor: theme.colors.gray[2],
+});
+
+export const scoreTimeline = styleVariants({
+  left: [
+    timeline,
+    {
+      backgroundColor: theme.colors.indicatorBlue[3],
+      color: theme.colors.white,
+    },
+  ],
+  right: [
+    timeline,
+    {
+      backgroundColor: theme.colors.indicatorRed[3],
+      color: theme.colors.white,
+    },
+  ],
+});
+
+export const rightSide = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.spaces.xs,
+});
+
+export const content = styleVariants({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spaces.xxs,
+  },
+  title: {
+    fontSize: theme.textVariants.xs.fontSize,
+  },
+  descriptionArea: {
+    fontSize: theme.textVariants.xxs.fontSize,
+  },
+  scoreArea: {
+    marginInline: theme.spaces.xxs,
+  },
 });
