3c0ad26c3ac134e63b0fdf4419f5e625ad69e971
Seongmin Choi <88662637+seongminn@users.noreply.github.com>
2024-03-29 10:55:01 +0900

    [FIX] cheer count 중복 요청하는 버그 수정 (#237)
    [FIX]: cheer count 중복 요청하는 버그 수정

--- apps/spectator/queries/useCheerMutation.ts
+++ apps/spectator/queries/useCheerMutation.ts
@@ -1,7 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { postCheer } from '@/api/game';
-import { GameCheerType } from '@/types/game';
 
 type CheerMutationOptions = {
   gameTeamId: number;
@@ -14,42 +13,6 @@ export default function useCheerMutation() {
 
   return useMutation({
     mutationFn: (params: CheerMutationOptions) => postCheer(params),
-    onMutate: async ({ gameId, gameTeamId, cheerCount }) => {
-      // 이전 데이터를 가져옴
-      const previousData = queryClient.getQueryData(['game-cheer', gameId]) as
-        | GameCheerType[]
-        | null;
-
-      // 요청 취소
-      await queryClient.cancelQueries({ queryKey: ['game-cheer', gameId] });
-
-      // 데이터 업데이트
-      if (previousData) {
-        const updatedData = previousData.map(item => {
-          if (item.gameTeamId === gameTeamId) {
-            return {
-              ...item,
-              cheerCount: item.cheerCount + cheerCount,
-            };
-          }
-
-          return item;
-        });
-
-        queryClient.setQueryData(['game-cheer', gameId], updatedData);
-      }
-
-      return { previousData };
-    },
-    onError: (err, variables, context) => {
-      // 에러 발생 시 이전 데이터로 롤백
-      if (context?.previousData) {
-        queryClient.setQueryData(
-          ['game-cheer', variables.gameId],
-          context.previousData,
-        );
-      }
-    },
     onSettled: (data, error, variables) => {
       // 쿼리 갱신
       queryClient.invalidateQueries({
