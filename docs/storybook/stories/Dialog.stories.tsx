014d1705442d40ec47174c224139458d4afdf10a
seongminn <berry9901@gmail.com>
2024-06-06 22:13:21 +0900

    docs: dialog stories 세분화

--- docs/storybook/stories/Dialog.stories.tsx
+++ docs/storybook/stories/Dialog.stories.tsx
@@ -1,37 +1,174 @@
-import { Dialog, DialogTrigger, DialogContent } from '@hcc/ui';
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogOverlay,
+  DialogClose,
+} from '@hcc/ui';
 import type { Meta, StoryObj } from '@storybook/react';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: '@hcc/Dialog',
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
 } satisfies Meta<typeof Dialog>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   render: () => {
     return (
       <Dialog>
         <DialogTrigger asChild>
           <button>click</button>
         </DialogTrigger>
-        <DialogContent>
+        <DialogContent style={{ maxWidth: 380 }}>
+          <DialogHeader>
+            <DialogTitle>다이얼로그 제목입니다.</DialogTitle>
+            <DialogDescription>다이얼로그 설명입니다.</DialogDescription>
+          </DialogHeader>
           <div>
-            <h2 style={{ marginTop: 0 }}>모달 제목</h2>
-            <p>
-              모달 내용이 여기에 표시됩니다.모달 내용이 여기에 표시됩니다.모달
-              내용이 여기에 표시됩니다.모달 내용이 여기에 표시됩니다.
-            </p>
+            모달 내용이 여기에 표시됩니다.모달 내용이 여기에 표시됩니다.모달
+            내용이 여기에 표시됩니다.모달 내용이 여기에 표시됩니다.
           </div>
+          <DialogFooter>
+            <button
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                backgroundColor: '#E9EBEE',
+                color: '#374553',
+                padding: '8px 0',
+                border: '1px solid transparent',
+                borderRadius: 6,
+              }}
+            >
+              취소
+            </button>
+            <button
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                backgroundColor: '#ECEEFF',
+                color: '#121E8A',
+                padding: '8px 0',
+                borderRadius: 6,
+                border: '1px solid transparent',
+              }}
+            >
+              확인
+            </button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  },
+};
+
+export const WithOverlay: Story = {
+  render: () => {
+    return (
+      <Dialog>
+        <DialogOverlay />
+        <DialogTrigger asChild>
+          <button>click</button>
+        </DialogTrigger>
+        <DialogContent style={{ maxWidth: 380 }}>
+          <DialogHeader>
+            <DialogTitle>다이얼로그 제목입니다.</DialogTitle>
+            <DialogDescription>다이얼로그 설명입니다.</DialogDescription>
+          </DialogHeader>
+          <div>
+            모달 내용이 여기에 표시됩니다.모달 내용이 여기에 표시됩니다.모달
+            내용이 여기에 표시됩니다.모달 내용이 여기에 표시됩니다.
+          </div>
+          <DialogFooter>
+            <button
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                backgroundColor: '#E9EBEE',
+                color: '#374553',
+                padding: '8px 0',
+                border: '1px solid transparent',
+                borderRadius: 6,
+              }}
+            >
+              취소
+            </button>
+            <button
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                backgroundColor: '#ECEEFF',
+                color: '#121E8A',
+                padding: '8px 0',
+                borderRadius: 6,
+                border: '1px solid transparent',
+              }}
+            >
+              확인
+            </button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  },
+};
+
+export const WithCloseButton: Story = {
+  render: () => {
+    return (
+      <Dialog>
+        <DialogOverlay />
+        <DialogTrigger asChild>
+          <button>click</button>
+        </DialogTrigger>
+        <DialogContent style={{ maxWidth: 380 }}>
+          <DialogClose />
+          <DialogHeader>
+            <DialogTitle>다이얼로그 제목입니다.</DialogTitle>
+            <DialogDescription>다이얼로그 설명입니다.</DialogDescription>
+          </DialogHeader>
+          <div>
+            모달 내용이 여기에 표시됩니다.모달 내용이 여기에 표시됩니다.모달
+            내용이 여기에 표시됩니다.모달 내용이 여기에 표시됩니다.
+          </div>
+          <DialogFooter>
+            <button
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                backgroundColor: '#E9EBEE',
+                color: '#374553',
+                padding: '8px 0',
+                border: '1px solid transparent',
+                borderRadius: 6,
+              }}
+            >
+              취소
+            </button>
+            <button
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                backgroundColor: '#ECEEFF',
+                color: '#121E8A',
+                padding: '8px 0',
+                borderRadius: 6,
+                border: '1px solid transparent',
+              }}
+            >
+              확인
+            </button>
+          </DialogFooter>
         </DialogContent>
       </Dialog>
     );
