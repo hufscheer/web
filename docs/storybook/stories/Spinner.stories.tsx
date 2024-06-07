42a108a667701e6b1cb60bae311379ca31fa95fa
seongminn <berry9901@gmail.com>
2024-06-07 12:35:09 +0900

    fix: 누락된 프로퍼티(argTypes) 추가

--- docs/storybook/stories/Spinner.stories.tsx
+++ docs/storybook/stories/Spinner.stories.tsx
@@ -8,6 +8,7 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
     size: { control: 'inline-radio', options: ['sm', 'md', 'lg'] },
   },
 } satisfies Meta<typeof Spinner>;
