9e524f71359dcd0c02801b472f3f7e949dfe2d89
seongminn <berry9901@gmail.com>
2024-06-18 18:04:14 +0900

    docs: form 내에서 사용하는 select 스토리 추가

--- docs/storybook/stories/Form.stories.tsx
+++ docs/storybook/stories/Form.stories.tsx
@@ -5,6 +5,13 @@ import {
   FormLabel,
   FormMessage,
   Input,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
   Toaster,
   toast,
 } from '@hcc/ui';
@@ -245,6 +252,131 @@ export const OnChangeForm: Story = {
               }}
             >
               회원가입
+            </button>
+          </form>
+        </Form>
+
+        <Toaster />
+      </>
+    );
+  },
+};
+
+const managerFormSchema = z.object({
+  TEAM_NAME: z.string().min(1, { message: '팀명을 입력해주세요.' }),
+  PLAYER: z.string().min(1, { message: '선수를 선택해주세요.' }),
+  QUARTER: z.string().min(1, { message: '쿼터를 선택해주세요.' }),
+});
+
+const managerFormValues = {
+  TEAM_NAME: '경영 바바예투',
+  PLAYER: '',
+  QUARTER: '',
+};
+
+type ManagerFormSchema = z.infer<typeof managerFormSchema>;
+
+export const ManagerForm: Story = {
+  render: () => {
+    const methods = useForm<ManagerFormSchema>({
+      resolver: zodResolver(managerFormSchema),
+      defaultValues: managerFormValues,
+      mode: 'onSubmit',
+    });
+
+    const handleChange = (target: keyof ManagerFormSchema, value: string) => {
+      methods.setValue(target, value, { shouldDirty: true });
+    };
+
+    const onSubmit = (data: ManagerFormSchema) => {
+      toast({
+        title: '입력한 값은',
+        description: `팀명: ${data.TEAM_NAME}, 선수: ${data.PLAYER}, 쿼터: ${data.QUARTER}입니다.`,
+      });
+    };
+
+    return (
+      <>
+        <Form {...methods}>
+          <form
+            onSubmit={methods.handleSubmit(onSubmit)}
+            style={{ display: 'flex', flexDirection: 'column', gap: 16 }}
+          >
+            <h2>상황</h2>
+            <FormField name="TEAM_NAME">
+              <FormLabel>팀명</FormLabel>
+              <FormControl>
+                <Input readOnly />
+              </FormControl>
+              <FormMessage />
+            </FormField>
+
+            <h2>득점 상세 정보</h2>
+
+            <FormField name="PLAYER">
+              <Select
+                onValueChange={value => handleChange('PLAYER', value)}
+                defaultValue={methods.getValues('PLAYER')}
+              >
+                <FormLabel>선수</FormLabel>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>득점</SelectLabel>
+                    <SelectItem value="강아지">강아지</SelectItem>
+                    <SelectItem value="고양이">고양이</SelectItem>
+                    <SelectItem value="푸바오">푸바오</SelectItem>
+                    <SelectItem value="짱구">짱구</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormField>
+
+            <FormField name="QUARTER">
+              <Select
+                onValueChange={value => handleChange('QUARTER', value)}
+                defaultValue={methods.getValues('QUARTER')}
+              >
+                <FormLabel>쿼터</FormLabel>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>쿼터</SelectLabel>
+                    <SelectItem value="16">16강</SelectItem>
+                    <SelectItem value="8">8강</SelectItem>
+                    <SelectItem value="4">4강</SelectItem>
+                    <SelectItem value="2">결승</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormField>
+
+            <button
+              style={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#141B21',
+                color: '#fff',
+                borderRadius: 8,
+                paddingBlock: 16,
+                marginTop: 8,
+              }}
+            >
+              득점 추가
             </button>
           </form>
         </Form>
