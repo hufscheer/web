f4fe6107b97407b1f53207c086f9132418e54915
seongminn <berry9901@gmail.com>
2024-06-21 21:34:41 +0900

    feat: popover 컴포넌트 추가

---
+++ packages/ui/src/popover/popover.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import * as PopoverPrimitive from '@radix-ui/react-popover';
+import { clsx } from 'clsx';
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
+
+import * as styles from './styles.css';
+
+const Popover = PopoverPrimitive.Root;
+
+const PopoverTrigger = PopoverPrimitive.Trigger;
+
+const PopoverContent = forwardRef<
+  ElementRef<typeof PopoverPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
+>(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
+    <PopoverPrimitive.Content
+      ref={ref}
+      align={align}
+      sideOffset={sideOffset}
+      className={clsx(styles.content, className)}
+      {...props}
+    />
+  </PopoverPrimitive.Portal>
+));
+PopoverContent.displayName = PopoverPrimitive.Content.displayName;
+
+export { Popover, PopoverTrigger, PopoverContent };
