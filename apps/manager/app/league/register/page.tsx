941097377f7ecac9941634b34c09bcd5512a8231
seongminn <berry9901@gmail.com>
2024-07-06 01:00:31 +0900

    refactor: league register page 수정

--- apps/manager/app/league/register/page.tsx
+++ apps/manager/app/league/register/page.tsx
@@ -1,53 +1,188 @@
 'use client';
 
-import { Stepper } from '@mantine/core';
-import { useState } from 'react';
+import { CalendarIcon } from '@hcc/icons';
+import {
+  Button,
+  Calendar,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Icon,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  toast,
+} from '@hcc/ui';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 import Layout from '@/components/Layout';
 
-import ConfirmModal from './_components/Confirm';
-import LeagueInfo from './_components/LeagueInfo';
-import LeagueTeam from './_components/LeagueTeam';
-import LeagueTeamPlayers from './_components/LeagueTeamPlayers';
-import { stepperResolver } from './resolvers';
+import * as styles from './styles.css';
 
-export default function Register() {
-  const [leagueId, setLeagueId] = useState<number>(-1);
-  const [teamId, setTeamId] = useState<number>(-1);
+const formSchema = z.object({
+  leagueName: z.string().min(1, { message: '대회명을 입력해주세요' }),
+  startDate: z.date({ message: '시작일을 입력해주세요' }),
+  endDate: z.date().nullable(),
+  round: z.string({ message: '라운드를 입력해주세요' }),
+});
 
-  const [active, setActive] = useState(0);
-  const handleLeagueId = (step: number) => {
-    setActive(step);
-  };
+const defaultValues = {
+  leagueName: '',
+  endDate: null,
+  round: '32',
+};
 
-  const RightButton = () => {
-    return <ConfirmModal />;
+type FormSchema = z.infer<typeof formSchema>;
+
+export default function Page() {
+  const methods = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  const onSubmit = (data: FormSchema) => {
+    toast({
+      title: '테스트용 대회 생성 메시지',
+      description: JSON.stringify(data),
+    });
   };
 
   return (
-    <Layout navigationTitle="대회 생성" navigationMenu={<RightButton />}>
-      <Stepper active={active} vars={stepperResolver}>
-        <Stepper.Step label="대회 정보">
-          <LeagueInfo
-            nextStep={() => handleLeagueId(1)}
-            handleLeagueId={(id: number) => setLeagueId(id)}
+    <Layout navigationTitle="신규 대회 만들기">
+      <Form {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)} style={{ flex: 1 }}>
+          <FormField
+            control={methods.control}
+            name="leagueName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>대회 이름</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </Stepper.Step>
 
-        <Stepper.Step label="대회 팀">
-          <LeagueTeam
-            leagueId={leagueId}
-            handleTeamId={(id: number) => setTeamId(id)}
-            nextStep={() => handleLeagueId(2)}
+          <FormField
+            control={methods.control}
+            name="startDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>시작일</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button colorScheme="outline" fullWidth justify="between">
+                        <span>
+                          {field.value
+                            ? new Date(field.value).toLocaleDateString()
+                            : ''}
+                        </span>
+                        <Icon source={CalendarIcon} />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      defaultValue={field.value}
+                      onChange={field.onChange}
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </Stepper.Step>
-        <Stepper.Step label="팀 선수">
-          <LeagueTeamPlayers
-            teamId={teamId}
-            prevStep={() => handleLeagueId(1)}
+
+          <FormField
+            control={methods.control}
+            name="endDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>종료일</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button colorScheme="outline" fullWidth justify="between">
+                        <span>
+                          {field.value
+                            ? new Date(field.value).toLocaleDateString()
+                            : ''}
+                        </span>
+                        <Icon source={CalendarIcon} />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      defaultValue={field.value}
+                      onChange={field.onChange}
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </Stepper.Step>
-      </Stepper>
+
+          <FormField
+            control={methods.control}
+            name="round"
+            render={({ field }) => (
+              <FormItem>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormLabel>진행 방식</FormLabel>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                  </FormControl>
+
+                  <SelectContent>
+                    <SelectItem value="32">32강</SelectItem>
+                    <SelectItem value="16">16강</SelectItem>
+                    <SelectItem value="8">8강</SelectItem>
+                    <SelectItem value="4">4강</SelectItem>
+                    <SelectItem value="2">결승</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button fullWidth className={styles.button}>
+            대회 만들기
+          </Button>
+        </form>
+      </Form>
+
+      <div className={styles.tipBox}>
+        <div className={styles.tipTitle}>
+          <span className={styles.emoji}>🙌🏻</span>
+          <span>새로운 대회에 팀을 추가하는 방법</span>
+        </div>
+        <p className={styles.tipDescription}>
+          신규 대회를 만든 뒤 참가 팀 관리 탭에서 팀 생성과 <br /> 편집을 할 수
+          있어요.
+        </p>
+      </div>
     </Layout>
   );
 }
---
+++ apps/manager/app/league/register/styles.css.ts
@@ -0,0 +1,31 @@
+import { rem, theme } from '@hcc/styles';
+import { style } from '@vanilla-extract/css';
+
+export const button = style({
+  marginTop: rem(18),
+});
+
+export const tipBox = style({
+  backgroundColor: '#FBFBFC',
+
+  paddingBlock: rem(26),
+  paddingInline: rem(39),
+});
+
+export const tipTitle = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: rem(6),
+
+  fontWeight: 'bold',
+  marginBottom: rem(8),
+});
+
+export const emoji = style({
+  fontSize: rem(14),
+});
+
+export const tipDescription = style({
+  fontSize: rem(14),
+  color: theme.colors.black300,
+});
