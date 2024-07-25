1f0245ed5ca561e60c4fe6d1077c6cbf90fb7b23
ohprettyhak <haklee.googl@gmail.com>
2024-07-25 11:53:30 +0900

    feat: useGames 쿼리 파라미터 추가

--- packages/api/src/queries/useGames.ts
+++ packages/api/src/queries/useGames.ts
@@ -3,7 +3,16 @@ import { useQuery } from '@tanstack/react-query';
 import { queryKeys } from '../queryKey';
 import { StateType } from '../types';
 
-const useGames = (leagueId: string, state: StateType) =>
-  useQuery(queryKeys.games(leagueId, state));
+const useGames = (
+  league_id: string,
+  state: StateType,
+  cursor?: number,
+  size?: number,
+  league_team_id?: number,
+  round?: number,
+) =>
+  useQuery(
+    queryKeys.games(league_id, state, cursor, size, league_team_id, round),
+  );
 
 export default useGames;
