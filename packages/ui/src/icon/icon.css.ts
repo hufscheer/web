c31b8eae5c525b5761bff1f73cc26d38341b84a6
ohprettyhak <haklee.googl@gmail.com>
2024-08-28 12:29:20 +0900

    chore: icon 컴포넌트에 color variant 사용

--- packages/ui/src/icon/icon.css.ts
+++ packages/ui/src/icon/icon.css.ts
@@ -1,18 +1,22 @@
+import { theme } from '@hcc/styles';
+import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
+const base = style({
+  color: theme.colors.gray900,
+});
+
+const color = {
+  primary: style({ color: theme.colors.primary.normal }),
+  secondary: style({ color: theme.colors.secondary.normal }),
+  black: style({ color: theme.colors.black }),
+  blue: style({ color: theme.colors.blue600 }),
+  gray: style({ color: theme.colors.gray200 }),
+  red: style({ color: theme.colors.red600 }),
+  white: style({ color: theme.colors.white }),
+};
+
 export const icon = recipe({
-  base: {
-    color: 'var(--hcc-colors-gray-900)',
-  },
-  variants: {
-    color: {
-      primary: { color: 'var(--hcc-colors-primary)' },
-      secondary: { color: 'var(--hcc-colors-secondary)' },
-      black: { color: 'var(--hcc-colors-black)' },
-      blue: { color: 'var(--hcc-colors-blue-600)' },
-      gray: { color: 'var(--hcc-colors-gray-200)' },
-      red: { color: 'var(--hcc-colors-red-600)' },
-      white: { color: 'var(--hcc-colors-white)' },
-    },
-  },
+  base,
+  variants: { color },
 });
