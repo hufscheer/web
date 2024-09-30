a38c7107d08957a5e4499a64fe29b377bb20b8f7
Seongmin Choi <berry9901@gmail.com>
2024-10-01 00:17:27 +0900

    feat(spectator): timeline ui 수정

--- apps/spectator/app/game/[id]/_components/Timeline/index.tsx
+++ apps/spectator/app/game/[id]/_components/Timeline/index.tsx
@@ -1,26 +1,62 @@
-import { useTimelineById } from '@/queries/useTimelineById';
+import { useGame, useTimeline } from '@hcc/api';
+import { Spinner } from '@hcc/ui';
+import { Fragment } from 'react';
+
 import { NotFoundError } from '@/services/errors';
 
-import Quarter from './Quarter';
+import EventRecord from './EventRecord';
+import TextRecord from './TextRecord';
 import * as styles from './Timeline.css';
+import { getProgressSemantics } from './Timeline.utils';
 
 type TimelineProps = {
   gameId: string;
 };
 
 export default function Timeline({ gameId }: TimelineProps) {
-  const { data: timelines } = useTimelineById(gameId);
+  const { data: game } = useGame(gameId);
+  const { data: timelines } = useTimeline(gameId);
+
+  if (!game || !timelines) return <Spinner />;
+
+  // TODO FallBack
+  if (!timelines) return <div>에러 메시지</div>;
 
   if (timelines.length === 0)
     throw new NotFoundError(
       '경기가 시작한 뒤 시간 순으로 타임라인이 업데이트됩니다.',
     );
 
+  const homeTeamId: number = game?.gameTeams[0].gameTeamId;
+
   return (
-    <div className={styles.root}>
-      {timelines.map(timeline => (
-        <Quarter key={timeline.gameQuarter} {...timeline} />
-      ))}
-    </div>
+    <Fragment>
+      {timelines.map(timeline => {
+        return (
+          <ul key={timeline.gameQuarter} className={styles.root}>
+            {timeline.records.map(record => {
+              if (record.progressRecord?.gameProgressType)
+                return (
+                  <TextRecord key={record.recordId} showDividerLine={true}>
+                    {timeline.gameQuarter}이(가)&nbsp;
+                    {getProgressSemantics(
+                      record.progressRecord.gameProgressType,
+                    )}
+                    되었습니다.
+                  </TextRecord>
+                );
+
+              return (
+                <EventRecord
+                  key={record.recordId}
+                  record={record}
+                  homeTeamId={homeTeamId}
+                />
+              );
+            })}
+          </ul>
+        );
+      })}
+    </Fragment>
   );
 }
