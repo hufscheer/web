83ccfe702fa0a30a39e3c1e9240da25ef0081d80
Hak Lee <haklee.googl@gmail.com>
2025-02-01 15:37:49 +0900

    build: 리액트 패키지 업데이트 (#358)
    * chore: update react and nextjs
    
    * chore: chromatic workflow 삭제

--- docs/storybook/scripts/chromatic_publish.sh
+++
@@ -1,2 +0,0 @@
-CHROMATIC_PROJECT_TOKEN=$(grep CHROMATIC_PROJECT_TOKEN .env | cut -d "=" -f2)
-pnpm dlx chromatic --project-token=$CHROMATIC_PROJECT_TOKEN --exit-once-uploaded --allow-console-errors --build-script-name=build
