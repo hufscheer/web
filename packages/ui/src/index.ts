3ae916ff826f957f2819e46cfeaabff8177024e3
ohprettyhak <me@haklee.me>
2025-02-17 12:24:23 +0900

    chore: Modal primitive 컴포넌트 제거

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,6 @@
 export { default as Accordion } from './accordion';
 export { default as Icon } from './icon';
 export { default as Uploader } from './image-uploader';
-export { default as Modal } from './modal';
 export { default as Skeleton } from './skeleton';
 export { default as Tabs } from './tabs';
 export { default as Tooltip } from './tooltip';
