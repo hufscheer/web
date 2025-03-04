505cab45fc7597215bc18994fc8516a40cd5b1c8
ohprettyhak <me@haklee.me>
2025-03-05 00:18:25 +0900

    feat: 페이지 로딩 ProgressBar 추가

--- apps/spectator/app/Providers.tsx
+++ apps/spectator/app/Providers.tsx
@@ -1,8 +1,11 @@
 'use client';
+
+import { ProgressProvider } from '@bprogress/next/app';
 import {
   QueryClientProvider as HccQueryClientProvider,
   ReactQueryDevtools as HccReactQueryDevtools,
 } from '@hcc/api';
+import { theme } from '@hcc/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren, useState } from 'react';
 
@@ -23,7 +26,16 @@ const Providers = ({ children }: PropsWithChildren) => {
 
   return (
     <HccQueryClientProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <ProgressProvider
+          height="2px"
+          color={theme.colors.blue600}
+          options={{ showSpinner: false }}
+          shallowRouting={false}
+        >
+          {children}
+        </ProgressProvider>
+      </QueryClientProvider>
       <HccReactQueryDevtools />
     </HccQueryClientProvider>
   );
