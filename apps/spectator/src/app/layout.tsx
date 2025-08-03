1ab3e125088eeebc697ae5eb3090ed54d2c6a28e
ohprettyhak <me@haklee.me>
2025-08-04 01:32:28 +0900

    feat: initialize Next.js project with TypeScript configuration and basic layout

---
+++ apps/spectator/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import type { PropsWithChildren } from 'react';
+import { Pretendard } from './_fonts';
+
+export const metadata: Metadata = {
+  title: '훕치치',
+  description: '한국외대 스포츠 플랫폼 훕치치',
+};
+
+const RootLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <html lang="ko">
+      <body className={Pretendard.className}>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ apps/spectator/src/app/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Home</div>;
+};
+
+export default Page;
