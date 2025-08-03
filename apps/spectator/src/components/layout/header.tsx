5858107cbc3a9eb009177d92538f4b500fab7d7a
ohprettyhak <me@haklee.me>
2025-08-04 02:23:57 +0900

    feat: update global styles and header component with new logo and colors

--- apps/spectator/src/components/layout/header.tsx
+++ apps/spectator/src/components/layout/header.tsx
@@ -1,3 +1,12 @@
+import { HCCLogo } from '@hcc/icons';
+import Link from 'next/link';
+
 export const Header = () => {
-  return <header>header</header>;
+  return (
+    <header className="center sticky top-0 z-header h-[44px] w-full border-neutral-50 border-b">
+      <Link href="/">
+        <HCCLogo width="71.5" height="21" className="text-primary-500" />
+      </Link>
+    </header>
+  );
 };
