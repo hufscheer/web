35c5d49aab8d01b908b9e4a80d568d8dd5b66083
ohprettyhak <me@haklee.me>
2025-08-05 23:55:05 +0900

    feat: update header component height from 44px to 12

--- apps/spectator/src/components/layout/header.tsx
+++ apps/spectator/src/components/layout/header.tsx
@@ -4,7 +4,7 @@ import { ROUTES } from '~/constants/routes';
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 z-header row-between h-[44px] w-full border-neutral-100 border-b px-5">
+    <header className="sticky top-0 z-header row-between h-12 w-full border-neutral-100 border-b px-5">
       <Link href={ROUTES.HOME}>
         <HCCLogo width="71.5" height="21" className="text-primary-500" />
       </Link>
