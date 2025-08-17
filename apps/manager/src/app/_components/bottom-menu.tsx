661c0a6ffe345263c870cfef318a9f8b44d16a63
ohprettyhak <me@haklee.me>
2025-08-17 17:12:03 +0900

    feat(ui): improve spacing and formatting in BottomMenu component

--- apps/manager/src/app/_components/bottom-menu.tsx
+++ apps/manager/src/app/_components/bottom-menu.tsx
@@ -8,19 +8,17 @@ export const BottomMenu = () => {
     <div className="row-between w-full gap-2 border-neutral-100 border-t bg-white p-5">
       <Button className="flex-1 gap-1" variant="subtle" asChild>
         <Link href={ROUTES.LEAGUE}>
-          <RewardedAdsIcon size={20} />
-          대회 관리
+          <RewardedAdsIcon size={20} /> 대회 관리
         </Link>
       </Button>
       <Button className="flex-1 gap-1" variant="subtle" asChild>
         <Link href={ROUTES.PLAYER}>
-          <PersonIcon size={20} />
-          선수 관리
+          <PersonIcon size={20} /> 선수 관리
         </Link>
       </Button>
       <Button className="flex-1 gap-1" variant="subtle" asChild>
         <Link href={ROUTES.TEAM}>
-          <GroupIcon size={20} />팀 관리
+          <GroupIcon size={20} /> 팀 관리
         </Link>
       </Button>
     </div>
