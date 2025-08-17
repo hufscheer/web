ad786f928cef4dc6a4e174c6b13bd007a881ed28
ohprettyhak <me@haklee.me>
2025-08-17 23:54:05 +0900

    feat: add player management page with edit functionality

---
+++ apps/manager/src/app/players/page.tsx
@@ -0,0 +1,36 @@
+import { Typography } from '@hcc/ui';
+import { Suspense } from '@suspensive/react';
+import Link from 'next/link';
+import { Header } from '~/components/layout';
+import { ROUTES } from '~/constants/routes';
+
+const PlayerEditButton = ({ edit }: { edit: boolean }) => {
+  return (
+    <Typography color="var(--color-neutral-500)" weight="semibold" asChild>
+      <Link href={`${ROUTES.PLAYER}?edit=${edit ? 'false' : 'true'}`} replace>
+        {edit ? '완료' : '편집'}
+      </Link>
+    </Typography>
+  );
+};
+
+type Props = {
+  searchParams: Promise<{ edit?: string }>;
+};
+
+const Page = async ({ searchParams }: Props) => {
+  const { edit: _edit } = await searchParams;
+  const edit = _edit === 'true';
+
+  return (
+    <>
+      <Header title="선수 관리" menu={<PlayerEditButton edit={edit} />} arrow />
+
+      <div className="column mt-1.5 h-full gap-1.5">
+        <Suspense clientOnly />
+      </div>
+    </>
+  );
+};
+
+export default Page;
