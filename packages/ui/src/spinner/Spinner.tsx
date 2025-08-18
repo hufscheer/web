ae69278d148f6d9d16ff23aec2ca7ab20d4f96cb
ohprettyhak <me@haklee.me>
2025-08-18 22:55:34 +0900

    feat: add spinner component with customizable size and color, and enhance color definitions

---
+++ packages/ui/src/spinner/Spinner.tsx
@@ -0,0 +1,56 @@
+import { Slot } from '@radix-ui/react-slot';
+import { type ComponentPropsWithoutRef, forwardRef } from 'react';
+import { match } from 'ts-pattern';
+import { colors } from '../token';
+import styles from './Spinner.module.css';
+
+type SpinnerSize = 'lg' | 'md';
+
+type SpinnerColor = 'primary' | 'neutral' | 'white';
+
+export interface SpinnerProps extends Omit<ComponentPropsWithoutRef<'div'>, 'size' | 'color'> {
+  asChild?: boolean;
+  size?: SpinnerSize;
+  color?: SpinnerColor;
+}
+
+export const Spinner = forwardRef<HTMLDivElement, SpinnerProps>(
+  ({ asChild, size = 'md', color = 'primary', className, style: _style, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'div';
+    const style = {
+      ..._style,
+      '--hcc-spinner-size': `${getSize(size)}px`,
+      '--hcc-spinner-border-width': `${getBorderWidth(size)}px`,
+      '--hcc-spinner-border-color': getBorderColor(color),
+      '--hcc-spinner-bg-color': getBackgroundColor(color),
+    };
+
+    return <Comp ref={ref} className={styles.spinner} style={style} {...props} />;
+  },
+);
+
+const getSize = (size: SpinnerSize) =>
+  match(size)
+    .with('lg', () => 40)
+    .with('md', () => 24)
+    .exhaustive();
+
+const getBorderWidth = (size: SpinnerSize) =>
+  match(size)
+    .with('lg', () => 5)
+    .with('md', () => 3)
+    .exhaustive();
+
+const getBorderColor = (color: SpinnerColor) =>
+  match(color)
+    .with('primary', () => colors.primary600)
+    .with('neutral', () => colors.neutral600)
+    .with('white', () => colors.white)
+    .exhaustive();
+
+const getBackgroundColor = (color: SpinnerColor) =>
+  match(color)
+    .with('primary', () => colors.primary200)
+    .with('neutral', () => colors.neutral200)
+    .with('white', () => colors.whiteAlpha30)
+    .exhaustive();
