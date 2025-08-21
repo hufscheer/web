f1eb6e148f6f8d1f27b6d2568245e4c740b5dc6b
ohprettyhak <me@haklee.me>
2025-08-21 15:17:13 +0900

    feat: add response hook to handle 401 errors and redirect to login

--- packages/api-base/src/fetcher.ts
+++ packages/api-base/src/fetcher.ts
@@ -12,6 +12,17 @@ const defaultOption: Options = {
 export const instance = ky.create({
   prefixUrl: API_URL,
   headers: { 'Content-Type': 'application/json' },
+  hooks: {
+    afterResponse: [
+      async (request, _, response) => {
+        if (!response.ok && response.status === 401 && !request.url.includes('logout')) {
+          alert('로그인이 만료되었어요. 다시 로그인해주세요.');
+          window.location.href = '/auth/login';
+        }
+        return response;
+      },
+    ],
+  },
   ...defaultOption,
 });
 
