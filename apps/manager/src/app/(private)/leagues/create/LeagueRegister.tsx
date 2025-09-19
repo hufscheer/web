f1f3d48ed2c78584fc4448525ce921fae306bc62
sungwonnoh <jessica20730@naver.com>
2025-09-20 01:16:24 +0900

    feat(manager): 참가 팀 선택 화면 ui개발

--- apps/manager/src/app/(private)/leagues/create/LeagueRegister.tsx
+++ apps/manager/src/app/(private)/leagues/create/LeagueRegister.tsx
@@ -1,26 +1,104 @@
 'use client';
-import { Button, Input } from '@hcc/ui';
-import { Suspense } from '@suspensive/react';
+
+import { AddIcon, CloseIcon, TradeHorizontalIcon } from '@hcc/icons';
+import { Button, Input, Typography } from '@hcc/ui';
+import { useState } from 'react';
+import { Drawer } from 'vaul';
+import { SelectTeam } from '../_components/select-team';
+
+type Team = { id: number; name: string };
+type Affiliation = { id: number; name: string; teams: Team[] };
+type RegisteredTeam = {
+  affiliationName: string;
+  teamName: string;
+  teamId: number;
+};
 
 type Props = {
   onPrev: () => void;
 };
+
 const LeagueRegister = ({ onPrev }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [registeredTeams, setRegisteredTeams] = useState<RegisteredTeam[]>([]);
+
+  const handleRegisterTeam = ({ affiliation, team }: { affiliation: Affiliation; team: Team }) => {
+    // 중복 등록 방지
+    if (!registeredTeams.find(rt => rt.teamId === team.id)) {
+      setRegisteredTeams(prevTeams => [
+        ...prevTeams,
+        {
+          affiliationName: affiliation.name,
+          teamName: team.name,
+          teamId: team.id,
+        },
+      ]);
+    }
+    setIsOpen(false);
+  };
+
+  const handleRemoveTeam = (teamId: number) => {
+    setRegisteredTeams(prevTeams => prevTeams.filter(team => team.teamId !== teamId));
+  };
+
   return (
-    <div className="column h-full gap-1.5 bg-white p-5">
-      <Suspense clientOnly>
-        <div className="flex flex-col gap-4">
+    <Drawer.Root open={isOpen} onOpenChange={setIsOpen}>
+      <div className="flex h-full flex-col gap-4 bg-white p-5">
+        <div className="flex flex-row items-center justify-between">
           <div className="font-semibold text-black text-lg">참가 팀</div>
+          <span className="flex flex-row font-semibold text-base">
+            <Typography color="var(--color-primary-600)" weight="semibold">
+              {registeredTeams.length}
+            </Typography>
+            /32
+          </span>
         </div>
 
-        <Button size="lg" className="w-full" color="primary" onClick={onPrev}>
-          이전 단계
-        </Button>
-        <Button size="lg" className="w-full" color="primary">
-          대회 생성
-        </Button>
-      </Suspense>
-    </div>
+        <div className="flex flex-row flex-wrap gap-3">
+          {registeredTeams.map(team => (
+            <Button
+              key={team.teamId}
+              variant="subtle"
+              onClick={() => handleRemoveTeam(team.teamId)}
+            >
+              <div className="flex flex-row items-center gap-2 p-3">
+                <span>{team.teamName}</span>
+                <CloseIcon width={16} height={16} color="var(--color-primary-600)" />
+              </div>
+            </Button>
+          ))}
+        </div>
+
+        <Drawer.Trigger asChild>
+          <Button size="lg" color="black" variant="subtle" className="w-full">
+            <AddIcon />
+            새로운 팀 추가
+          </Button>
+        </Drawer.Trigger>
+        <div className="mt-auto flex flex-col gap-2">
+          <Button size="lg" variant="subtle" className="w-full" onClick={onPrev}>
+            이전 단계
+          </Button>
+          <Button size="lg" className="w-full">
+            대회 생성
+          </Button>
+        </div>
+      </div>
+
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+        <Drawer.Content className="fixed right-0 bottom-0 left-0 mt-24 flex flex-col rounded-t-lg bg-white">
+          <div className="flex-1 rounded-t-lg p-4">
+            <div className="mx-auto mb-4 h-1.5 w-12 flex-shrink-0 rounded-full bg-gray-300" />
+            <Drawer.Title className="mb-4 text-start font-semibold text-2xl">팀 선택</Drawer.Title>
+            <Input size="lg" placeholder="팀 이름을 검색해주세요">
+              {/* 아이콘추가 */}
+            </Input>
+            <SelectTeam onClose={() => setIsOpen(false)} onRegister={handleRegisterTeam} />
+          </div>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 };
 
---
+++ apps/manager/src/components/ui/bottom-sheet.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { Drawer } from 'vaul';
+import React, { type ComponentProps } from 'react';
+
+type BottomSheetProps = ComponentProps<typeof Drawer.Root> & {
+  title: string;
+};
+
+export const BottomSheet = ({ children, title, ...props }: BottomSheetProps) => {
+  const childrenArray = React.Children.toArray(children);
+
+  const trigger = childrenArray[0];
+  const content = childrenArray[1];
+
+  return (
+    <Drawer.Root {...props}>
+      {trigger}
+
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+        <Drawer.Content className="fixed right-0 bottom-0 left-0 mt-24 flex flex-col rounded-t-[10px] bg-white">
+          <div className="flex-1 rounded-t-[10px] p-4">
+            <div className="mx-auto mb-4 h-1.5 w-12 flex-shrink-0 rounded-full bg-gray-300" />
+            <Drawer.Title className="mb-4 text-center font-semibold text-lg">{title}</Drawer.Title>
+            {content}
+          </div>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
+  );
+};
