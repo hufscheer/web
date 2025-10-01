4731e865862882566e2b2fd9caa2aa27b5a594fa
Hak Lee <hi@haklee.me>
2025-10-01 16:47:25 +0900

    chore(nextjs): `Next.js` 이미지 최적화 비활성화 설정 (#435)
    * chore: disable image optimization in Next.js configuration
    
    * chore: enable unoptimized image loading in Next.js configuration

--- apps/spectator/next.config.ts
+++ apps/spectator/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
