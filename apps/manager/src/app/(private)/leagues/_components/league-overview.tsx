dbae65fdabed16ba33d5397716fbd71667001100
sungwonnoh <jessica20730@naver.com>
2025-12-22 10:37:11 +0900

    fix(manager): 대회 관리 버튼 라우팅 추가

--- apps/manager/src/app/(private)/leagues/_components/league-overview.tsx
+++ apps/manager/src/app/(private)/leagues/_components/league-overview.tsx
@@ -14,7 +14,7 @@ export const LeagueOverview = () => {
   return (
     <Fragment>
       {data.map(league => (
-        <div key={league.id} className="w-full bg-white p-5">
+        <Link href={`/${routes.league(league.id)}`} key={league.id} className="w-full bg-white p-5">
           <div className="row-between w-full bg-white">
             <div className="center-y gap-1.5">
               <Badge size="sm" variant={league.leagueProgress === '진행 중' ? 'danger' : 'default'}>
@@ -22,9 +22,9 @@ export const LeagueOverview = () => {
               </Badge>
               <Typography weight="semibold">{league.name}</Typography>
             </div>
-            <Link className="center" href={`/${routes.league(league.id)}`}>
+            <div className="center">
               <ChevronForwardIcon size={24} />
-            </Link>
+            </div>
           </div>
 
           <hr className="my-4 h-[1px] w-full border-none bg-neutral-100" />
@@ -58,13 +58,13 @@ export const LeagueOverview = () => {
 
           <div className="row-between mt-4 gap-2.5">
             <Button className="flex-1" size="sm" color="black" variant="subtle" asChild>
-              <Link href={''}>참가 팀 관리</Link>
+              <Link href={'/teams'}>참가 팀 관리</Link>
             </Button>
             <Button className="flex-1" size="sm" color="black" variant="subtle" asChild>
-              <Link href={''}>응원톡 관리</Link>
+              <Link href={'/cheertalks'}>응원톡 관리</Link>
             </Button>
           </div>
-        </div>
+        </Link>
       ))}
     </Fragment>
   );
