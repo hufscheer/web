83c7dbb3b809b97ca3fee3c3143751edb4750031
노성원 <129041262+sungwonnoh@users.noreply.github.com>
2025-12-22 16:33:13 +0900

    fix(manager): 대회 관리 버튼 라우팅 추가 (#438)
    * fix(manager): 대회 관리 버튼 라우팅 추가
    
    * fix(manager): 중첩link를 제거하고 button라우팅으로 변경

--- apps/manager/src/app/(private)/leagues/_components/league-overview.tsx
+++ apps/manager/src/app/(private)/leagues/_components/league-overview.tsx
@@ -4,17 +4,18 @@ import { ChevronForwardIcon } from '@hcc/icons';
 import { formatTime } from '@hcc/toolkit';
 import { Badge, Button, Typography } from '@hcc/ui';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { Fragment } from 'react';
 import { useSuspenseLeaguesLeague } from '~/api';
 import { routes } from '~/constants/routes';
 
 export const LeagueOverview = () => {
   const { data } = useSuspenseLeaguesLeague();
-
+  const router = useRouter();
   return (
     <Fragment>
       {data.map(league => (
-        <div key={league.id} className="w-full bg-white p-5">
+        <Link href={`/${routes.league(league.id)}`} key={league.id} className="w-full bg-white p-5">
           <div className="row-between w-full bg-white">
             <div className="center-y gap-1.5">
               <Badge size="sm" variant={league.leagueProgress === '진행 중' ? 'danger' : 'default'}>
@@ -22,9 +23,9 @@ export const LeagueOverview = () => {
               </Badge>
               <Typography weight="semibold">{league.name}</Typography>
             </div>
-            <Link className="center" href={`/${routes.league(league.id)}`}>
+            <div className="center">
               <ChevronForwardIcon size={24} />
-            </Link>
+            </div>
           </div>
 
           <hr className="my-4 h-[1px] w-full border-none bg-neutral-100" />
@@ -57,14 +58,32 @@ export const LeagueOverview = () => {
           </div>
 
           <div className="row-between mt-4 gap-2.5">
-            <Button className="flex-1" size="sm" color="black" variant="subtle" asChild>
-              <Link href={''}>참가 팀 관리</Link>
+            <Button
+              className="flex-1"
+              size="sm"
+              color="black"
+              variant="subtle"
+              onClick={e => {
+                e.stopPropagation();
+                router.push(`/${routes.teams}`);
+              }}
+            >
+              참가 팀 관리
             </Button>
-            <Button className="flex-1" size="sm" color="black" variant="subtle" asChild>
-              <Link href={''}>응원톡 관리</Link>
+            <Button
+              className="flex-1"
+              size="sm"
+              color="black"
+              variant="subtle"
+              onClick={e => {
+                e.stopPropagation();
+                router.push(`/${routes.cheertalks}`);
+              }}
+            >
+              응원톡 관리
             </Button>
           </div>
-        </div>
+        </Link>
       ))}
     </Fragment>
   );
