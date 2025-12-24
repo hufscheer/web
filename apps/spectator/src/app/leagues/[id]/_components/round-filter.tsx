79746c5dd6db3488b112018bcba1e8e3fed1ccc3
sungwonnoh <jessica20730@naver.com>
2025-12-24 09:38:58 +0900

    fix(spectator): 페이지 랜딩시 탭과 경기 내용 연결되도록 로직 수정

--- apps/spectator/src/app/leagues/[id]/_components/round-filter.tsx
+++ apps/spectator/src/app/leagues/[id]/_components/round-filter.tsx
@@ -14,7 +14,7 @@ export const RoundFilter = ({ league, round }: Props) => {
   );
 
   return (
-    <Tabs.Root className="sticky top-12 z-header h-12 w-full" defaultValue={rounds[0].toString()}>
+    <Tabs.Root className="sticky top-12 z-header h-12 w-full" defaultValue={round.toString()}>
       <Tabs.List className="center h-12 gap-5 border-neutral-100 border-b bg-white">
         {rounds.map(r => (
           <TabTrigger className="min-w-14" key={r} value={r.toString()} queryKey="round">
