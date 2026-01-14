01c6b20d04df96556297cc6c9fcc388adcf871ff
sungwonnoh <jessica20730@naver.com>
2026-01-14 17:56:53 +0900

    feat(spectator): 팀별 트로피 진열장 ui

---
+++ apps/spectator/src/app/teams/[id]/_components/trophy.tsx
@@ -0,0 +1,21 @@
+import type { FC } from 'react';
+import type { TrophyType } from '~/api/types/teams';
+
+type Props = {
+  trophies?: TrophyType[];
+};
+
+export const TeamTrophy: FC<Props> = ({ trophies }) => {
+  if (!trophies || trophies.length === 0) return null;
+
+  return (
+    <div className="scrollbar-hide flex w-full flex-row items-center justify-center gap-2 overflow-x-auto rounded-xl bg-neutral-100 px-2 py-1">
+      {trophies.map(trophy => (
+        <div key={trophy.leagueId} className="flex-shrink-0 flex-col items-center gap-2 px-2 py-1">
+          <div className="text-center text-3xl">🏆</div>
+          <div className="whitespace-nowrap text-neutral-700 text-xs">{trophy.leagueName}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
