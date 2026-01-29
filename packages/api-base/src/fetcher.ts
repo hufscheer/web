6ad6b5da6ebfac7e9120407c782ca2f70b381159
sungwonnoh <jessica20730@naver.com>
2026-01-29 14:17:29 +0900

    fix(manager): 첫 화면 진입시 엑세스토큰 알림창 에러 문제해결

--- packages/api-base/src/fetcher.ts
+++ packages/api-base/src/fetcher.ts
@@ -15,7 +15,16 @@ export const instance = ky.create({
   hooks: {
     afterResponse: [
       async (request, _, response) => {
-        if (!response.ok && response.status === 401 && !request.url.includes('logout')) {
+        if (!response.ok && response.status === 401) {
+          if (request.url.includes('logout')) return response;
+
+          const currentPath = window.location.pathname;
+
+          const isInternalNavigation = document?.referrer?.includes(window.location.host);
+          if (currentPath === '/' && !isInternalNavigation) {
+            window.location.href = '/auth/login';
+            return response;
+          }
           alert('로그인이 만료되었어요. 다시 로그인해주세요.');
           window.location.href = '/auth/login';
         }
