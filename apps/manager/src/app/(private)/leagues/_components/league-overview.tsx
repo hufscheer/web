d47f8fb559fb15d8c1d66ec3fb2bc55f19f71099
sungwonnoh <jessica20730@naver.com>
2026-03-25 21:53:40 +0900

    fix: 버튼 라우팅 오류 수정

--- apps/manager/src/app/(private)/leagues/_components/league-overview.tsx
+++ apps/manager/src/app/(private)/leagues/_components/league-overview.tsx
@@ -16,47 +16,52 @@ export const LeagueOverview = () => {
   return (
     <Fragment>
       {data.map((league) => (
-        <Link href={`/${routes.league(league.id)}`} key={league.id} className="w-full bg-white p-5">
-          <div className="row-between w-full bg-white">
-            <div className="center-y gap-1.5">
-              <Badge size="sm" variant={league.leagueProgress === '진행 중' ? 'danger' : 'default'}>
-                {league.leagueProgress}
-              </Badge>
-              <Typography weight="semibold">{league.name}</Typography>
+        <div key={league.id} className="w-full bg-white p-5">
+          <Link href={`/${routes.league(league.id)}`} className="block">
+            <div className="row-between w-full bg-white">
+              <div className="center-y gap-1.5">
+                <Badge
+                  size="sm"
+                  variant={league.leagueProgress === '진행 중' ? 'danger' : 'default'}
+                >
+                  {league.leagueProgress}
+                </Badge>
+                <Typography weight="semibold">{league.name}</Typography>
+              </div>
+              <div className="center">
+                <ChevronForwardIcon size={24} />
+              </div>
             </div>
-            <div className="center">
-              <ChevronForwardIcon size={24} />
+
+            <hr className="my-4 h-[1px] w-full border-none bg-neutral-100" />
+
+            <div className="column w-full gap-2.5">
+              <Typography
+                color="var(--color-neutral-500)"
+                fontSize={14}
+                weight="medium"
+                lineHeight="none"
+              >
+                <strong>참여</strong> {league.sizeOfLeagueTeams}개 팀
+              </Typography>
+              <Typography
+                color="var(--color-neutral-500)"
+                fontSize={14}
+                weight="medium"
+                lineHeight="none"
+              >
+                <strong>라운드</strong> {league.maxRound}강
+              </Typography>
+              <Typography
+                color="var(--color-neutral-500)"
+                fontSize={14}
+                weight="medium"
+                lineHeight="none"
+              >
+                <strong>기간</strong> {formatTime(league.startAt)} - {formatTime(league.endAt)}
+              </Typography>
             </div>
-          </div>
-
-          <hr className="my-4 h-[1px] w-full border-none bg-neutral-100" />
-
-          <div className="column w-full gap-2.5">
-            <Typography
-              color="var(--color-neutral-500)"
-              fontSize={14}
-              weight="medium"
-              lineHeight="none"
-            >
-              <strong>참여</strong> {league.sizeOfLeagueTeams}개 팀
-            </Typography>
-            <Typography
-              color="var(--color-neutral-500)"
-              fontSize={14}
-              weight="medium"
-              lineHeight="none"
-            >
-              <strong>라운드</strong> {league.maxRound}강
-            </Typography>
-            <Typography
-              color="var(--color-neutral-500)"
-              fontSize={14}
-              weight="medium"
-              lineHeight="none"
-            >
-              <strong>기간</strong> {formatTime(league.startAt)} - {formatTime(league.endAt)}
-            </Typography>
-          </div>
+          </Link>
 
           <div className="row-between mt-4 gap-2.5">
             <Button
@@ -64,10 +69,7 @@ export const LeagueOverview = () => {
               size="sm"
               color="black"
               variant="subtle"
-              onClick={(e) => {
-                e.stopPropagation();
-                router.push(`/${routes.teams}`);
-              }}
+              onClick={() => router.push(`/${routes.teams}`)}
             >
               참가 팀 관리
             </Button>
@@ -76,15 +78,12 @@ export const LeagueOverview = () => {
               size="sm"
               color="black"
               variant="subtle"
-              onClick={(e) => {
-                e.stopPropagation();
-                router.push(`/${routes.cheertalks}`);
-              }}
+              onClick={() => router.push(`/${routes.cheertalks}`)}
             >
               응원톡 관리
             </Button>
           </div>
-        </Link>
+        </div>
       ))}
     </Fragment>
   );
