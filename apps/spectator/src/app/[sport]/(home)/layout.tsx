1a91e7638295cd5ed81d37a0e27a95853f5b1acc
seongminn <berry9901@gmail.com>
2026-04-16 23:49:45 +0900

    chore: calendar 메뉴 제거

--- apps/spectator/src/app/[sport]/(home)/layout.tsx
+++ apps/spectator/src/app/[sport]/(home)/layout.tsx
@@ -2,24 +2,17 @@ import '@hcc/ui/styles.css';
 import '~/styles/globals.css';
 import type { PropsWithChildren } from 'react';
 
-import type { SportType } from '~/api/types';
-
 import { Header } from '~/components/layout';
-import { DEFAULT_SPORT, normalizeSportParam } from '~/utils/sport-route';
 
-import { CalendarMenu } from './_components/calendar-menu';
 import { NavigationBar } from './_components/navigation-bar';
 import { SchoolSelect } from './_components/school-select';
 
 type Props = PropsWithChildren<{ params: Promise<{ sport: string }> }>;
 
-const RootLayout = async ({ children, params }: Props) => {
-  const { sport } = await params;
-  const sportType: SportType = normalizeSportParam(sport) ?? DEFAULT_SPORT;
-
+const RootLayout = async ({ children }: Props) => {
   return (
     <div className="relative">
-      <Header center={<SchoolSelect />} menu={<CalendarMenu sport={sportType} />} />
+      <Header center={<SchoolSelect />} />
 
       <div className="flex flex-1 flex-col pb-navbar-height">{children}</div>
 
