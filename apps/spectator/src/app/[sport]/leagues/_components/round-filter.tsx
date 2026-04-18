d27cca4d071cf051d8d34d78d69a7d17a56f2549
Seongmin Choi <berry9901@gmail.com>
2026-04-18 22:55:20 +0900

    fix: 라운드 개선 (#500)
    * fix: 라우팅 문제 해결
    
    * refactor: 라우팅 구조 전면 개편
    
    * fix: ts error
    
    * chore: calendar 메뉴 제거
    
    * fix: 탭 리로딩 분리
    
    * fix: 예상치 못한 라운드 발생 방지
    
    * fix: 2강 제거

--- apps/spectator/src/app/[sport]/leagues/_components/round-filter.tsx
+++ apps/spectator/src/app/[sport]/leagues/_components/round-filter.tsx
@@ -10,20 +10,41 @@ type Props = {
 };
 
 export const RoundFilter = ({ league, round }: Props) => {
-  const rounds = Array.from(
-    { length: Math.floor(Math.log2(league.maxRound)) },
-    (_, i) => league.maxRound / 2 ** i,
-  );
+  const getLabel = (r: number) => {
+    if (r > 16) return '예선';
+    if (r === 2) return '결승';
+    return `${r}강`;
+  };
+
+  const rounds = getRounds(league);
 
   return (
     <Tabs.Root className="sticky top-12 z-header h-12 w-full" defaultValue={round.toString()}>
       <Tabs.List className="center h-12 gap-5 border-b border-neutral-100 bg-white">
         {rounds.map((r) => (
           <TabTrigger className="min-w-14" key={r} value={r.toString()} queryKey="round">
-            {r > 2 ? `${r}강` : '결승'}
+            {getLabel(r)}
           </TabTrigger>
         ))}
       </Tabs.List>
     </Tabs.Root>
   );
+};
+
+const getRounds = (league: LeagueDetailType) => {
+  const rounds: number[] = [];
+
+  // 예선 (16 이상일 경우)
+  if (league.maxRound > 16) {
+    rounds.push(league.maxRound);
+  }
+
+  // 16강, 8강, 4강, 결승
+  [16, 8, 4, 2].forEach((r) => {
+    if (r <= league.maxRound) {
+      rounds.push(r);
+    }
+  });
+
+  return rounds;
 };
