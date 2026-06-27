0920523970a5dcb92df42cf0da2a586e85ee704f
seongminn <berry9901@gmail.com>
2026-06-27 16:54:38 +0900

    build: rollup 제거

--- packages/ui/rollup.config.cjs
+++
@@ -1,6 +0,0 @@
-const { createUIRollupConfig } = require('@hcc/rollup-config/ui');
-const { vanillaExtractPlugin } = require('@vanilla-extract/rollup-plugin');
-
-module.exports = createUIRollupConfig({
-  plugins: [vanillaExtractPlugin()],
-});
