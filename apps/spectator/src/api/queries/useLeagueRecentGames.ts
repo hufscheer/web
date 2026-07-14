7ab63b8dbbfc8a2ca1dc8b9d9ce520d1fdcad23f
seongminn <berry9901@gmail.com>
2026-07-15 00:46:04 +0900

    fix: add organizationId params

--- apps/spectator/src/api/queries/useLeagueRecentGames.ts
+++ apps/spectator/src/api/queries/useLeagueRecentGames.ts
@@ -7,8 +7,10 @@ import { queryKeys } from '../queryKey';
 export const useLeagueRecentGame = (payload?: { sportType?: SportType }) =>
   useQuery(queryKeys.leagues.recentGames(payload));
 
-export const useSuspenseLeagueRecentGames = (payload?: { sportType?: SportType }) =>
-  useSuspenseQuery(queryKeys.leagues.recentGames(payload));
+export const useSuspenseLeagueRecentGames = (payload?: {
+  sportType?: SportType;
+  organizationId?: number;
+}) => useSuspenseQuery(queryKeys.leagues.recentGames(payload));
 
 export const fetchLeagueRecentGames = async (payload?: { sportType?: SportType }) =>
   await getQueryClient().fetchQuery(queryKeys.leagues.recentGames(payload));
