f15d2f4bc45bebdae9f42ccbafd788c8721eeb1c
seongminn <berry9901@gmail.com>
2026-09-05 01:24:59 +0900

    feat: lineup UI 수정

--- apps/spectator/src/app/org/[orgId]/soccer/games/_components/lineup-tab/index.tsx
+++ apps/spectator/src/app/org/[orgId]/soccer/games/_components/lineup-tab/index.tsx
@@ -1,5 +1,4 @@
 import { CandidateList } from './candidate-list';
-import { Ground } from './ground';
 import { PlayerList } from './player-list';
 
 type Props = {
@@ -9,10 +8,6 @@ type Props = {
 export const LineupTab = ({ gameId }: Props) => {
   return (
     <div className="bg-white pb-5">
-      <Ground gameId={gameId} />
-
-      <hr className="h-2 w-full border-none bg-neutral-50" />
-
       <PlayerList gameId={gameId} />
       <CandidateList gameId={gameId} />
     </div>
